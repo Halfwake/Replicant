ec36f1eb6952bdb044f3fbd963e21318c3de8d79
Andrew Dudash <drewd8@vt.edu>
2017-10-25 16:10:34 -0400

    Change interface of function and add documentation.

--- lib/Replicant/Network.pm
+++ lib/Replicant/Network.pm
@@ -24,11 +24,49 @@ use LWP::UserAgent;
 
 our @EXPORT = qw(fetch_robot_lines);
 
-sub fetch_robot_lines ($url) {
-    my $user_agent = LWP::UserAgent->new;
-    $user_agent->agent($user_agent_name);
+sub fetch_robot_lines ($url, $user_agent = 0) {
+    unless ($user_agent) {
+	my $user_agent = LWP::UserAgent->new;
+	$user_agent->agent($user_agent_name);
+    }
     my $robots_txt = $user_agent->get($url . '/robots.txt')->content;
     split(/\n/, $robots_txt);
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Replicant::Network - the network part of getting robot rules
+
+=head1 SYNOPSIS
+
+  ## Get the file 'http://localhost/robots.txt'
+  @lines = fetch_robot_lines("http://localhost/");
+
+  ## Get the file 'http://localhost/robots.txt' using the given LWP UserAgent.
+  my $user_agent = LWP::UserAgent->new;
+  @lines = fetch_robot_lines("http://localhost/", $user_agent);
+
+=head1 DESCRIPTION
+
+Functions in here are for retrieving a 'robots.txt' file for further processing.
+
+=head1 FUNCTIONS
+
+=head2 fetch_robot_lines
+
+  @lines = fetch_robot_lines('http://localhost/', LWP::UserAgent->new);
+  
+This will return a list of the rule lines in a robots file. All empty lines will be removed. The first argument is the root of the website you want to retrieve the file from, not the URL of the 'robots.txt' file.
+
+  fetch_robot_lines('http://localhost/')           #Correct!
+  fetch_robot_lines('http://localhost/robots.txt') #Wrong!
+
+The second argument is an optional LWP or LWP comptaible user agent object. If you provide one, it will be used to download the robot file. If you do not provide an argument, then a default user agent will be used.
+
+=cut
